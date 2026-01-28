9ab324266847f8927c75a5fc6fe18096c597d123
Ashan_madusanka <136890819+AshanMadusanka0@users.noreply.github.com>
2026-01-28 15:33:24 +0530

    The Dependences and Structure were added -new update

---
+++ Event Managment Backend/ticketing-service/src/main/java/com/eventmanagment/ticketing/client/EventServiceClient.java
@@ -0,0 +1,4 @@
+package com.eventmanagement.ticketing.client;
+
+public class EventServiceClient {
+}
---
+++ Event Managment Backend/ticketing-service/src/main/java/com/eventmanagment/ticketing/config/RestTemplateConfig.java
@@ -0,0 +1,4 @@
+package com.eventmanagement.ticketing.config;
+
+public class RestTemplateConfig {
+}
