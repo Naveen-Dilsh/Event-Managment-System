c3ed82ffe2924caa2ea41baab87427d3277d6180
Naveen-Dilsh <naveentype2001@gmail.com>
2026-01-29 07:41:35 +0530

    Implement Rest Api endpoint for get All Events and get Event By Id

--- Event Managment Backend/event-service/src/main/java/com/eventmanagment/event/service/impl/EventServiceImpl.java
+++ Event Managment Backend/event-service/src/main/java/com/eventmanagment/event/service/impl/EventServiceImpl.java
@@ -3,13 +3,21 @@ package com.eventmanagment.event.service.impl;
 import com.eventmanagment.event.dto.EventRequestDTO;
 import com.eventmanagment.event.dto.EventResponseDTO;
 import com.eventmanagment.event.entity.Event;
+import com.eventmanagment.event.exception.EventNotFoundException;
 import com.eventmanagment.event.helper.EventMapper;
 import com.eventmanagment.event.repository.EventRepository;
 import com.eventmanagment.event.service.EventService;
 
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 
 
 @Slf4j
@@ -41,4 +49,24 @@ public class EventServiceImpl implements EventService {
 
     }
 
+    @Override
+    @Transactional(readOnly = true)
+    public List<EventResponseDTO> getAllEvents(){
+        log.info("Fetching All Events");
+
+        List<Event> events = eventRepository.findAll();
+        return events.stream()
+                .map(eventMapper::mapToResponseDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    @Override
+    @Transactional(readOnly = true)
+    public EventResponseDTO getEventById(Long id){
+        log.info("Fetching Event With Id : {}", id);
+        Event event = eventRepository.findById(id)
+                .orElseThrow();
+        return eventMapper.mapToResponseDTO(event);
+    }
 }
